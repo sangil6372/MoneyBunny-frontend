6e2a26dd19d16f179b58a4ce5ac4bcd0eade6461
sangil6372 <sangil6372@naver.com>
2025-07-16 17:21:34 +0000

    chore: 초기 PWA 설정(manifest, 서비스워커, 아이콘 등)

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import './assets/main.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'vue-awesome-paginate/dist/style.css'; // 페이지네이션 스타일
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "vue-awesome-paginate/dist/style.css"; // 페이지네이션 스타일
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import VueAwesomePaginate from 'vue-awesome-paginate'; // 페이지네이션 컴포넌트
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import VueAwesomePaginate from "vue-awesome-paginate"; // 페이지네이션 컴포넌트
 
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import router from "./router";
+import "./registerServiceWorker";
 
 const app = createApp(App);
 
@@ -15,4 +16,4 @@ app.use(VueAwesomePaginate); // 전역 컴포넌트 등록
 app.use(createPinia());
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
