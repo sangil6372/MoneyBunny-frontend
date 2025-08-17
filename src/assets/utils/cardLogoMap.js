dfeee2fc1840d41f0cd80d6b3190cb88d9027407
sangil6372 <sangil6372@naver.com>
2025-08-17 02:25:29 +0000

    Fix: 이미지 경로 하드 코딩 수정

--- src/assets/utils/cardLogoMap.js
+++ src/assets/utils/cardLogoMap.js
@@ -2,20 +2,20 @@ import cardCodeMap from './cardCodeMap.js';
 
 // 카드사 전용 로고맵
 export const cardLogoMap = {
-  KB카드: '/src/assets/images/icons/card/kb.png',
-  현대카드: '/src/assets/images/icons/card/hyundai.png',
-  삼성카드: '/src/assets/images/icons/card/samsung.png',
-  NH카드: '/src/assets/images/icons/card/nh.png',
-  BC카드: '/src/assets/images/icons/card/bc.png',
-  신한카드: '/src/assets/images/icons/card/shinhan.png',
-  씨티카드: '/src/assets/images/icons/card/citi.png',
-  우리카드: '/src/assets/images/icons/card/woori.png',
-  롯데카드: '/src/assets/images/icons/card/lotte.png',
-  하나카드: '/src/assets/images/icons/card/hana.png',
-  전북카드: '/src/assets/images/icons/card/jeonbuk.png',
-  //  광주카드: '/src/assets/images/icons/card/gwangju.png',
-  수협카드: '/src/assets/images/icons/card/suhyup.png',
-  제주카드: '/src/assets/images/icons/card/jeju.png',
+  KB카드: new URL('@/assets/images/icons/card/kb.png', import.meta.url).href,
+  현대카드: new URL('@/assets/images/icons/card/hyundai.png', import.meta.url).href,
+  삼성카드: new URL('@/assets/images/icons/card/samsung.png', import.meta.url).href,
+  NH카드: new URL('@/assets/images/icons/card/nh.png', import.meta.url).href,
+  BC카드: new URL('@/assets/images/icons/card/bc.png', import.meta.url).href,
+  신한카드: new URL('@/assets/images/icons/card/shinhan.png', import.meta.url).href,
+  씨티카드: new URL('@/assets/images/icons/card/citi.png', import.meta.url).href,
+  우리카드: new URL('@/assets/images/icons/card/woori.png', import.meta.url).href,
+  롯데카드: new URL('@/assets/images/icons/card/lotte.png', import.meta.url).href,
+  하나카드: new URL('@/assets/images/icons/card/hana.png', import.meta.url).href,
+  전북카드: new URL('@/assets/images/icons/card/jeonbuk.png', import.meta.url).href,
+  //  광주카드: new URL('@/assets/images/icons/card/gwangju.png', import.meta.url).href,
+  수협카드: new URL('@/assets/images/icons/card/suhyup.png', import.meta.url).href,
+  제주카드: new URL('@/assets/images/icons/card/jeju.png', import.meta.url).href,
 };
 
 export function getCardLogo(cardCompanyOrCode) {
